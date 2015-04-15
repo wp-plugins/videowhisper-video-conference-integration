1515d8853b923d81367df03d929b2dbce7f02565
videowhisper <videowhisper@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-15 16:16:58 +0000

    Update
    git-svn-id: https://plugins.svn.wordpress.org/videowhisper-video-conference-integration/trunk@1135568 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- vc/vw_upload.php
+++ vc/vw_upload.php
@@ -1,26 +1,32 @@
 <?php
+include("inc.php");
+if (!is_user_logged_in())
+{
+echo 'loadstatus=0';
+exit; //only logged in users can upload
+}
 
-if ($_GET["room"]) $room=$_GET["room"];
-if ($_POST["room"]) $room=$_POST["room"];
+if ($_GET['room']) $room=$_GET['room'];
+if ($_POST['room']) $room=$_POST['room'];
 $filename=$_FILES['vw_file']['name'];
 
-include_once("incsan.php");
+include_once('incsan.php');
 sanV($room);
 if (!$room) exit;
-sanV($filename); //sanitised
+sanV($filename); //sanitise
 if (!$filename) exit;
-
 
 //suppress uploads to other folders or containing .php
 if (strstr(strtolower($filename),'.php')) exit;
-if ( strstr($room,"/") || strstr($room,"..") ) exit;
-if ( strstr($filename,"/") || strstr($filename,"..") ) exit;
+if (strstr($room,'/') || strstr($room,'..') ) exit;
+if (strstr($filename,'/') || strstr($filename,'..') ) exit;
+if ($room[0]=='.' || $filename[0]=='.') exit;
 
-$destination="uploads/".$room."/";
-if ($_GET["slides"]) $destination .= "slides/";
+$destination='uploads/'.$room.'/';
+if ($_GET['slides']) $destination .= 'slides/';
 
 $ext = strtolower(pathinfo($filename, PATHINFO_EXTENSION));
-$allowed = array("swf","zip","rar","jpg","jpeg","png","gif","txt","doc","docx","pdf","mp3","mp4", "flv", "avi", "mpg", "ppt","pptx", "pps", "ppsx", "doc", "docx", "odt", "odf", "rtf", "xls", "xlsx");
+$allowed = array('zip','rar','jpg','jpeg','png','gif','txt','doc','docx','pdf','mp4', 'flv', 'avi', 'mpg', 'ppt','pptx', 'pps', 'ppsx', 'doc', 'docx', 'odt', 'odf', 'rtf', 'xls', 'xlsx');
 
 if (in_array($ext,$allowed)) move_uploaded_file($_FILES['vw_file']['tmp_name'], $destination . $filename);
 ?>loadstatus=1
